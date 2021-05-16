5a7e18d2e0546e3457dd1407bf5a581a9e88b03a
Code Sandbox <hello@codesandbox.io>
2021-05-16 11:52:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e5e5aeeabaec32d56c7c4c213c3cb4bb.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c64be9e77b5bfd1625ee97c85ab0c0",
+    "url": "https://mahaveerpatil.github.io/csb-jktfj/index.html"
+  },
+  {
+    "revision": "533b3380298681bdc9dd",
+    "url": "https://mahaveerpatil.github.io/csb-jktfj/static/js/main.e9aa682b.chunk.js"
+  },
+  {
+    "revision": "62c1258efd65a2f956d2",
+    "url": "https://mahaveerpatil.github.io/csb-jktfj/static/js/runtime-main.12223e90.js"
+  }
+]);
